df1d3524e6b470a946260e5c78e82d9a6c924ec0
Meelis Karulin <m.karulin@syzygy.de>
2018-05-30 16:58:55 +0200

    Revert "[BUGFIX] - Get the hash of the file contents instead of just the name."
    This reverts commit bdc14e05408c7646215bbc5b35ba0c7fee8e6597.

--- Classes/Driver/AmazonS3Driver.php
+++ Classes/Driver/AmazonS3Driver.php
@@ -559,20 +559,8 @@ class AmazonS3Driver extends TYPO3\CMS\Core\Resource\Driver\AbstractHierarchical
     public function hash($fileIdentifier, $hashAlgorithm)
     {
         $fileIdentifier = $this->canonicalizeAndCheckFileIdentifier($fileIdentifier);
-        $path = $this->getStreamWrapperPath($fileIdentifier);
 
-        switch ($hashAlgorithm) {
-            case 'sha1':
-                $hash = sha1_file($path);
-                break;
-            case 'md5':
-                $hash = md5_file($path);
-                break;
-            default:
-                throw new \RuntimeException('Hash algorithm ' . $hashAlgorithm . ' is not implemented.', 1329644451);
-        }
-
-        return $hash;
+        return sha1($fileIdentifier);
     }
 
     /**
